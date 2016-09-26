a0610cb4599be78f6631b88b4fa00d318a03ace4
afunx <afun1989@gmail.com>
2016-09-26 17:37:47 +0800

    CRC8.java is added and tested

--- src/com/espressif/iot/esptouch/test/JUnitTest.java
+++ src/com/espressif/iot/esptouch/test/JUnitTest.java
@@ -1,10 +1,27 @@
 package com.espressif.iot.esptouch.test;
 
+import com.espressif.iot.esptouch.util.CRC8;
+
 import android.test.AndroidTestCase;
 
 @SuppressWarnings("deprecation")
 public class JUnitTest extends AndroidTestCase {
 
-	public void test() {
+	public void testCRC8() {
+		CRC8 crc8 = new CRC8();
+		byte[] bytes = new byte[] { 1, 3, 8, 11, -2, -100 };
+		crc8.update(bytes);
+		assertEquals(83, crc8.getValue());
+		
+		crc8.reset();
+		crc8.update(103);
+		crc8.update(-53);
+		crc8.update(25);
+		assertEquals(73,crc8.getValue());
+		
+		crc8.reset();
+		bytes = new byte[] { 23, -34, 127, -1, 0, 25 };
+		crc8.update(bytes, 2, 3);
+		assertEquals(49,crc8.getValue());
 	}
 }
