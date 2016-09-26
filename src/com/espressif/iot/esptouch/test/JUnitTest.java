863e6deb5f5eed80ae854b8a6f9feba1e874e9f6
afunx <afun1989@gmail.com>
2016-09-26 17:05:05 +0800

    JUnit is added

---
+++ src/com/espressif/iot/esptouch/test/JUnitTest.java
@@ -0,0 +1,10 @@
+package com.espressif.iot.esptouch.test;
+
+import android.test.AndroidTestCase;
+
+@SuppressWarnings("deprecation")
+public class JUnitTest extends AndroidTestCase {
+
+	public void test() {
+	}
+}
